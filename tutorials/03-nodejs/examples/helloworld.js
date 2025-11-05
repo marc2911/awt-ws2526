174b4efd4e6c834f120ce8cb06aa01e84112174a
Fichtner, Marc <marc.a.fichtner@gmail.com>
2025-11-05 13:01:18 +0100

    03

--- tutorials/03-nodejs/examples/helloworld.js
+++ tutorials/03-nodejs/examples/helloworld.js
@@ -1,8 +1,9 @@
-const http = require('http');
+import { createServer } from 'http';
+
 const port = 8976;
 const host = '127.0.0.1';
 
-const server = http.createServer((request, response) => {
+const server = createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Hello World\n');
 });
