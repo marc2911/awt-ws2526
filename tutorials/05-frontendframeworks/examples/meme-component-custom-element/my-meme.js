ca65c451680ab11148a04a0bd089affacd8e8276
Fichtner, Marc <marc.a.fichtner@gmail.com>
2025-11-19 12:55:54 +0100

    05

--- tutorials/05-frontendframeworks/examples/meme-component-custom-element/my-meme.js
+++ tutorials/05-frontendframeworks/examples/meme-component-custom-element/my-meme.js
@@ -1,30 +1,30 @@
-(function() {
-
+(function () {
     // we inherit HTMLElement to create our own HTML element
     class MyMeme extends HTMLElement {
-
         // connectedCallback() fires when the element is inserted into the DOM
         connectedCallback() {
-            this.attachShadow({mode: 'open'});
+            this.attachShadow({ mode: "open" });
             // Import the shared template
             this.shadowRoot.appendChild(template.content.cloneNode(true));
 
             // Cache the paragraph and image elements
-            this.captionTopParagraph = this.shadowRoot.querySelector('p.captionTop');
-            this.captionBottomParagraph = this.shadowRoot.querySelector('p.captionBottom');
-            this.imageElement = this.shadowRoot.querySelector('img');
+            this.captionTopParagraph =
+                this.shadowRoot.querySelector("p.captionTop");
+            this.captionBottomParagraph =
+                this.shadowRoot.querySelector("p.captionBottom");
+            this.imageElement = this.shadowRoot.querySelector("img");
 
             // Checks for attribute values initially
-            if (this.hasAttribute('captionTop')) {
-                var captionTop = this.getAttribute('captionTop');
+            if (this.hasAttribute("captionTop")) {
+                var captionTop = this.getAttribute("captionTop");
                 this.setCaptionTop(captionTop);
             }
-            if(this.hasAttribute('captionBottom')) {
-                var captionBottom = this.getAttribute('captionBottom');
+            if (this.hasAttribute("captionBottom")) {
+                var captionBottom = this.getAttribute("captionBottom");
                 this.setCaptionBottom(captionBottom);
             }
-            if(this.hasAttribute('memeTemplateUrl')) {
-                var memeTemplateUrl = this.getAttribute('memeTemplateUrl');
+            if (this.hasAttribute("memeTemplateUrl")) {
+                var memeTemplateUrl = this.getAttribute("memeTemplateUrl");
                 this.setMemeTemplateUrl(memeTemplateUrl);
             }
         }
@@ -32,13 +32,13 @@
         // attributeChangedCallback() is called when any of the attributes in the observedAttributes array are changed
         attributeChangedCallback(name, oldValue, newValue) {
             switch (name) {
-                case 'captionTop':
+                case "captionTop":
                     this.setCaptionTop(newValue);
                     break;
-                case 'captionBottom':
+                case "captionBottom":
                     this.setCaptionBottom(newValue);
                     break;
-                case 'memeTemplateUrl':
+                case "memeTemplateUrl":
                     this.setMemeTemplateUrl(newValue);
                     break;
             }
@@ -47,26 +47,25 @@
         setCaptionTop(val) {
             this.captionTop = val;
             this.captionTopParagraph.textContent = this.captionTop;
-        };
+        }
         setCaptionBottom(val) {
             this.captionBottom = val;
             this.captionBottomParagraph.textContent = this.captionBottom;
-        };
+        }
         setMemeTemplateUrl(val) {
             this.memeTemplateUrl = val;
             this.imageElement.src = this.memeTemplateUrl;
-        };
-
+        }
     }
 
     // To avoid invoking the parser with `.innerHTML` for every new instance, a
     // template for the contents of the shadow DOM is shared by all
     // <my-meme> instances.
-    const template = document.createElement('template');
-    template.innerHTML = `        
-        <img src=""/>    
+    const template = document.createElement("template");
+    template.innerHTML = `
+        <img src=""/>
         <p class="caption captionTop"></p>
-        <p class="caption captionBottom"></p> 
+        <p class="caption captionBottom"></p>
         <style>
             .caption{
                 position: absolute;
@@ -98,6 +97,5 @@
         </style>`;
 
     // register the new component in the web browser
-    window.customElements.define('my-meme', MyMeme);
-
+    window.customElements.define("my-meme", MyMeme);
 })();
