69cb79953560cd34f664229c6bc1ab7f93016072
Fichtner, Marc <marc.a.fichtner@gmail.com>
2025-11-20 13:08:51 +0100

    05 calculator create-react-app

---
+++ tutorials/05-frontendframeworks/calculator/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Calculator from "./Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <Calculator title={"Simple Calculator"} />
+    </div>
+  );
+}
+
+export default App;
---
+++ tutorials/05-frontendframeworks/calculator/src/Calculator.js
@@ -0,0 +1,11 @@
+/**
+ * @param {{ title: string }} props
+ */
+export default function Calculator({ title }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>This is a simple calculator app.</p>
+    </div>
+  );
+}
