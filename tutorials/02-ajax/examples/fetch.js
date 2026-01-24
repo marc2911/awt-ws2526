d10a13e3019cab44c8e805a011fce6c328b94e7e
Fichtner, Marc <marc.a.fichtner@gmail.com>
2026-01-24 15:53:23 +0100

    fetch

--- tutorials/02-ajax/examples/fetch.js
+++ tutorials/02-ajax/examples/fetch.js
@@ -4,9 +4,26 @@ handleResults = result => {
 const url = "https://httpbin.org/post";
 const data = { text: "Hello!" };
 
-fetch(url, {
-  method: "POST",
-  body: data
-})
-  .then(res => res.json())
-  .then(handleResults);
+function callbackPOST() {
+  fetch(url, {
+    method: "POST",
+    body: data
+  })
+    .then(res => res.json())
+    .then(handleResults);
+}
+
+async function promisePOST() {
+  const response = await fetch(url, {
+    method: "POST",
+    body: data
+  });
+  const result = await response.json();
+  handleResults(result);
+}
+
+async function promiseGET() {
+  const response = await fetch(url);
+  const result = await response.json();
+  handleResults(result);
+}
